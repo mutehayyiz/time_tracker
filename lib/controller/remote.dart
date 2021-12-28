adc3689ac57b5a7c949b1c162ab7b3e1c38e796f
mutehayyiz <ahmet_senharputlu@hotmail.com>
2021-12-28 03:47:20 +0300

    Add set url func

--- lib/controller/remote.dart
+++ lib/controller/remote.dart
@@ -5,13 +5,19 @@ import 'package:http/http.dart' as http;
 import 'package:time_tracker/model/total.dart';
 
 class Remote {
+  String host = "https://go-time-tracker-server.herokuapp.com";
+
+  setUrl(String path) {
+    return host + path;
+  }
+
   addCategory(Category c) async {
-    var url = Uri.parse('http://localhost:4242/category');
+    var url = Uri.parse(setUrl("/category"));
     await http.post(url, body: json.encode(c.toJson()));
   }
 
   Future<List<Category>> getCategories() async {
-    var url = Uri.parse("http://localhost:4242/category");
+    var url = Uri.parse(setUrl("/category"));
     final response = await http.get(url);
 
     return List<Category>.from(json
@@ -21,20 +27,19 @@ class Remote {
   }
 
   deleteCategory(Category c) async {
-    var url = Uri.parse('http://localhost:4242/category');
+    var url = Uri.parse(setUrl("/category"));
     await http.delete(url, body: json.encode(c.toJson()));
   }
 
   addEntry(Entry e) async {
-    var url = Uri.parse('http://localhost:4242/entry');
+    var url = Uri.parse(setUrl("/entry"));
     var response = await http.post(url, body: json.encode(e.toJson()));
     var id = json.decode(response.body)["id"];
     return id;
   }
 
   Future<List<Entry>> getEntries(String categoryName) async {
-    var url =
-        Uri.parse('http://localhost:4242/entry/' + categoryName.toLowerCase());
+    var url = Uri.parse(setUrl("/entry/" + categoryName.toLowerCase()));
     final response = await http.get(url);
 
     return List<Entry>.from(
@@ -42,8 +47,7 @@ class Remote {
   }
 
   Future<List<Entry>> getEntriesDaily(String categoryName, String date) async {
-    var url =
-        Uri.parse("http://localhost:4242/entry/" + categoryName + "/daily");
+    var url = Uri.parse(setUrl("/entry/" + categoryName + "/daily"));
     final response = await http.post(url, body: json.encode({"date": date}));
 
     return List<Entry>.from(
@@ -51,26 +55,25 @@ class Remote {
   }
 
   void removeEntry(String category, String id) async {
-    var url = Uri.parse(
-        'http://localhost:4242/entry/' + category.toLowerCase() + "/" + id);
+    var url = Uri.parse(setUrl('/entry/' + category.toLowerCase() + "/" + id));
     await http.delete(url);
   }
 
   Future<List<Total>> totalByDate(String date) async {
-    var url = Uri.parse("http://localhost:4242/total/daily");
+    var url = Uri.parse(setUrl("/total/daily"));
     final response = await http.post(url, body: json.encode({"date": date}));
     return List<Total>.from(
         json.decode(response.body).map((x) => Total.fromJson(x)).toList());
   }
 
   Future<List<String>> getDays() async {
-    var url = Uri.parse("http://localhost:4242/days");
+    var url = Uri.parse(setUrl("/days"));
     final response = await http.get(url);
     return List<String>.from(json.decode(response.body).map((x) => x)).toList();
   }
 
   Future<Map<String, List<Entry>>> getAllData() async {
-    var url = Uri.parse('http://localhost:4242/entry');
+    var url = Uri.parse(setUrl('/entry'));
     final response = await http.get(url);
 
     var data = jsonDecode(response.body) as Map;
